b90ff10af31680f6989c05efc8563cb4e2da1bd7
gabrildnsilva <gabrieldnsilva@gmail.com>
2025-09-05 20:42:09 -0300

    fix: fix elements in carousel

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,11 @@
-function changeBackground(bg, title) {
-	const banner = document.querySelector(".banner");
-	const contents = document.querySelectorAll(".content");
-	banner.style.background = `url('/src/assets/images/backgrounds/${bg}') no-repeat`;
-	banner.style.backgroundSize = "cover";
-	banner.style.backgroundPosition = "center";
-
-	contents.forEach((content) => {
-		content.classList.remove("active");
-		if (content.classList.contains(title)) {
-			content.classList.add("active");
-		}
+// Ready Carrousel out of HTML
+$(document).ready(function () {
+	$(".carousel").carousel({
+		dist: -100,
+		shift: 0,
+		padding: 20,
+		numVisible: 5,
+		indicators: true,
+		noWrap: false,
 	});
-}
+});
