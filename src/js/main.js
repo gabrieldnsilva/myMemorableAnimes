d5d6d1efa7189546bb5cb88cd83ae3e203ac1f34
gabrildnsilva <gabrieldnsilva@gmail.com>
2025-09-07 10:55:37 -0300

    fix: changeBackground at start carousel function. Avoiding null background

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,12 @@
 // Ready Carrousel out of HTML
 $(document).ready(function () {
 	$(".carousel").carousel({
-		dist: -100,
 		shift: 0,
 		padding: 20,
 		numVisible: 5,
 		indicators: true,
 		noWrap: false,
 	});
+
+	changeBackground("narutoShippuden-background.webp", "narutoShippuden");
 });
